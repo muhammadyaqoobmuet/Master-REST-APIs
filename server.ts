c36793685261aba74b1bf9f5a19599fd44c3dd6d
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-06 20:19:23 +0500

    Setup Enviroment with Profesional way

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
+import { config } from './src/config/config';
 import app from './src/app';
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.PORT || 3000;
   app.listen(port, () => {
     console.log(`server listens at ${port}`);
   });
