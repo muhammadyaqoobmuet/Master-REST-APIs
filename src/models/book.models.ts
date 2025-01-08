ad0d35d9ba8ea5bd68dbf4f38471e91f6b614eed
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-08 18:45:13 +0500

    create book model

---
+++ src/models/book.models.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import { IUser } from './user.models';
+// Interface for TypeScript
+interface IBooks extends Document {
+  _id: string;
+  title: string;
+  author: IUser;
+  genre: string;
+  coverImage: string;
+  file: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Define Schema
+const bookSchema: Schema<IBooks> = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: [true, 'Genre is required'],
+    },
+    coverImage: {
+      type: String,
+      required: false,
+    },
+    file: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+// Create and Export Model
+const Book: Model<IBooks> = mongoose.model<IBooks>('Book', bookSchema);
+
+export default Book;
