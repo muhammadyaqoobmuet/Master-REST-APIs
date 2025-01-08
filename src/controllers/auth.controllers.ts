e0c50193288d8d2586e7f04ba7d56dc3a85a8bc9
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-08 17:05:54 +0500

    added hashing function

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
-
+import User from '../models/user.models';
+import bcrypt from "bcrypt"
 export const registerUser = async (
   req: Request,
   res: Response,
@@ -14,25 +15,43 @@ export const registerUser = async (
   };
   console.log(name, email, password);
 
-  // validation 
+  // validation
   if (!name || !email || !password) {
     const createdError: createHttpError.HttpError<400> = createHttpError(
       400,
       'invaild input all feilds are required'
     );
-    return next(createdError)
+    return next(createdError);
   }
-
 
   // checking user in DB it already exists of not
 
   try {
-        
+    const user = await User.findOne({ email });
+
+    // checking if user exists or not 
+    if (user) {
+      const userExistsError = createHttpError(
+        400,
+        'user already exsists ! try to login  '
+      );
+      next(userExistsError);
+    }
+
+    // if flow is here means user does not exists
+
+    const hashedPassword = await bcrypt.hash(password,11);
+
+
+
+
+
+
+
   } catch (error) {
-    
+    next(error);
   }
 
-  
   res.status(200).json({
     message: 'user created LOL',
   });
